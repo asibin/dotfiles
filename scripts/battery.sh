e65351bc2ed74fd190eee73e3e406055d7cbd7e9
Sibin Arsenijević <sibin.arsenijevic@gmail.com>
2022-09-28 13:37:40 +0200

    updated battery script
    Fixed case where 100% battery wasn't reported properly
    Added option to run script with icons positional argument to display
    unicode battery icon instead of text Bat:
    Shortened text from Battery: to Bat: in order to save space on tmux
    statusbar

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+PERCENTAGE=$(pmset -g batt 2>&1 | egrep "1?[0-9][0-9]%" | awk '{print $3}' | sed 's/;//')
 
-pmset -g batt | grep "[0-9][0-9]%" | awk '{print $3}' | sed 's/;//'
+if [ "$PERCENTAGE" != '' ]; then
+    if [ "$1" == 'icons' ]; then
+        printf "\xF0\x9F\x94\x8B %s" "${PERCENTAGE}"
+    else
+        printf "Bat: %s" "${PERCENTAGE}"
+    fi
+else
+    # Silently fail
+    printf ''
+fi
+
